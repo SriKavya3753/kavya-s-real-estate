d484d679c816aff8f637a529929f5d3214803752
SriKavya3753 <msrikavya2004@gmail.com>
2025-02-10 22:39:17 +0530

    frontend updated upto the testimonals

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './components/Header/Header'
+import Hero from './components/Hero/hero';
+import Companies from './components/Companies/Companies';
+import Residencies from './components/Residencies/Residencies';
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient"></div>
+        <Header/>
+        <Hero/>
+      </div>
+      <Companies/>
+      <Residencies/>
+    </div>
+  );
+}
+
+export default App;
