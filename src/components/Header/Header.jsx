4358cc70a527026278ddef64493c7c5e2ad4b63a
SriKavya3753 <msrikavya2004@gmail.com>
2025-02-11 12:00:24 +0530

    frontend updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,80 +1,51 @@
-import "./Hero.css";
-import { HiLocationMarker } from "react-icons/hi";
-import CountUp from "react-countup";
-import { motion } from "framer-motion";
-const Hero = () => {
+import React, { useState } from "react";
+import "./Header.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import { getMenuStyles } from "../../utils/common";
+import useHeaderColor from "../../hooks/useHeaderColor";
+import OutsideClickHandler from "react-outside-click-handler";
+
+const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const headerColor = useHeaderColor();
+
   return (
-    <section className="hero-wrapper">
-      <div className="paddings innerWidth flexCenter hero-container">
-        {/* left side */}
-        <div className="flexColStart hero-left">
-          <div className="hero-title">
-            <div className="orange-circle" />
-            <motion.h1
-            initial={{ y: "2rem", opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{
-              duration: 2,
-              type: "ease-in",
-            }}
-            >
-              Discover <br />
-              Most Suitable
-              <br /> Property
-            </motion.h1>
-          </div>
-          <div className="flexColStart secondaryText flexhero-des">
-            <span>Find a variety of properties that suit you very easilty</span>
-            <span>Forget all difficulties in finding a residence for you</span>
-          </div>
+    <section className="h-wrapper" style={{ background: headerColor }}>
+      <div className="flexCenter innerWidth paddings h-container">
+        {/* logo */}
+        <img src="./logo.png" alt="logo" width={100} />
 
-          <div className="flexCenter search-bar">
-            <HiLocationMarker color="var(--blue)" size={25} />
-            <input type="text" />
-            <button className="button">Search</button>
-          </div>
-
-          <div className="flexCenter stats">
-            <div className="flexColCenter stat">
-              <span>
-                <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
-              </span>
-              <span className="secondaryText">Premium Product</span>
-            </div>
-
-            <div className="flexColCenter stat">
-              <span>
-                <CountUp start={1950} end={2000} duration={4} /> <span>+</span>
-              </span>
-              <span className="secondaryText">Happy Customer</span>
-            </div>
-
-            <div className="flexColCenter stat">
-              <span>
-                <CountUp end={28} /> <span>+</span>
-              </span>
-              <span className="secondaryText">Awards Winning</span>
-            </div>
-          </div>
-        </div>
-
-        {/* right side */}
-        <div className="flexCenter hero-right">
-          <motion.div
-            initial={{ x: "7rem", opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{
-              duration: 2,
-              type: "ease-in",
-            }}
-            className="image-container"
+        {/* menu */}
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setMenuOpened(false);
+          }}
+        >
+          <div
+            // ref={menuRef}
+            className="flexCenter h-menu"
+            style={getMenuStyles(menuOpened)}
           >
-            <img src="./hero-image.png" alt="houses" />
-          </motion.div>
+            <a href="#residencies">Residencies</a>
+            <a href="#value">Our Value</a>
+            <a href="#contact-us">Contact Us</a>
+            <a href="#get-started">Get Started</a>
+            <button className="button">
+              <a href="mailto:zainkeepscode@gmail.com">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
+
+        {/* for medium and small screens */}
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
         </div>
       </div>
     </section>
   );
 };
 
-export default Hero;
+export default Header;
